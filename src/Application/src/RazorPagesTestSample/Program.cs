be6b372f961bd17d5e89db39d0e99ae0a04f970a
yk26tan <yktk1019@gmail.com>
2024-08-29 12:27:22 +0900

    test (added comment)

--- src/Application/src/RazorPagesTestSample/Program.cs
+++ src/Application/src/RazorPagesTestSample/Program.cs
@@ -6,6 +6,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using RazorPagesTestSample.Data;
 
+//test comment to check CI
 namespace RazorPagesTestSample
 {
     public class Program
